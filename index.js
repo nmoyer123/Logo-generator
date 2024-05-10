2b8e39df3b385740a3c3bb8cbc8047a58e03c17f
Noah Moyer <noahmoyer@icloud.com>
2024-05-09 21:20:34 -0700

    adding test files and gitignore

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
-const inquire = require('inquirer');
-const question = require ('questions');
-const fs = require ('fs');
-//require {Circle, Triangle, Square} from "shape.js file is"
 
+
+// Import necessary modules
+const inquirer = require('inquirer');
+const fs = require('fs');
+// Import shape constructors from the shape.js file
+const { Circle, Triangle, Square } = require('./lib/shapes.js');
+
+// Define the questions for the command line interface
+const questions = [
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape would you like to use for your logo?',
+        choices: ['Circle', 'Triangle', 'Square']
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'What color should the shape be? (CSS colors)'
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter the text for your logo:'
+    }
+];
+
+// Prompt the user with questions
 inquirer.prompt(questions).then(answers => {
-    console.log("MADE IT HERE! MY ANSWERS: ", answers)
     let shape;
-    //if check shape is to be a circle
-    // shape = new Circle()
-    //shape = new Triangle()
-    // shape = new Square()
+    // Decide which shape to instantiate based on user input
+    switch(answers.shape) {
+        case 'Circle':
+            shape = new Circle();
+            break;
+        case 'Triangle':
+            shape = new Triangle();
+            break;
+        case 'Square':
+            shape = new Square();
+            break;
+    }
 
-    // shape.setColor()
-    //shape.setText()
+    // Set the color and text of the shape
+    shape.setColor(answers.color);
+    shape.setText(answers.text);
 
-    // var svgText = shape.render()
-    // fs.writeFile("logo.svg", svgText, (err) => {
-    //     if (err) {
-    //         console.log("There was an error: ", err)
-    //         return
-    //     }
-    //     console.log("Success!")
-       
+    // Render the shape to SVG
+    const svgText = shape.render();
 
-    // })
-
-})
+    // Write the SVG to a file
+    fs.writeFile("logo.svg", svgText, (err) => {
+        if (err) {
+            console.log("There was an error: ", err);
+            return;
+        }
+        console.log("SVG logo successfully created!");
+    });
+});
